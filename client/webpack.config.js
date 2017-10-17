a44c2a46d1be6d3f19b2db40635b7d6f020b0e74
johnvincentio <john@johnvincent.io>
2017-10-17 12:35:30 -0400

    tabs

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,3 @@
-
 const webpack = require('webpack');
 const path = require('path');
 
@@ -10,61 +9,59 @@ const DIST_FOLDER = path.resolve(APP_FOLDER, './dist');
 const DIST_FOLDER_STYLE = path.resolve(DIST_FOLDER, './css');
 
 const config = {
-  entry: ['./src/index.jsx', './src/scss/styles.scss'],
-  output: {
-    path: path.resolve('dist'),
-    filename: 'bundle.js',
-  },
+	entry: ['./src/index.jsx', './src/scss/styles.scss'],
+	output: {
+		path: path.resolve('dist'),
+		filename: 'bundle.js',
+	},
 
-  devtool: 'source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    inline: true,
-    port: 8025,
-  },
+	devtool: 'source-map',
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		inline: true,
+		port: 8025,
+	},
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      { // regular css files
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: 'style-loader/url!file-loader',
-          use: ['css-loader'],
-          publicPath: DIST_FOLDER_STYLE,
-        }),
-      },
-      { // sass / scss loader for webpack
-        test: /\.(sass|scss)$/,
-        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  plugins: [
-    new ExtractTextPlugin({ // define where to save the file
-      filename: '[name].bundle.css',
-      allChunks: true,
-    }),
-    new CopyWebpackPlugin(
-      [
-        { from: 'index.html', to: '.' },
-      ],
-      { debug: 'info' }
-    )
-  ],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+			{
+				// regular css files
+				test: /\.css$/,
+				loader: ExtractTextPlugin.extract({
+					fallback: 'style-loader/url!file-loader',
+					use: ['css-loader'],
+					publicPath: DIST_FOLDER_STYLE,
+				}),
+			},
+			{
+				// sass / scss loader for webpack
+				test: /\.(sass|scss)$/,
+				loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	plugins: [
+		new ExtractTextPlugin({
+			// define where to save the file
+			filename: '[name].bundle.css',
+			allChunks: true,
+		}),
+		new CopyWebpackPlugin([{ from: 'index.html', to: '.' }], { debug: 'info' }),
+	],
 };
 
 module.exports = config;
