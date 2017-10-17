66d080ce6624ef61f93805059bee56fb0575d04c
johnvincentio <john@johnvincent.io>
2017-10-17 10:57:59 -0400

    http fetch

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -28,3 +28,32 @@ export const DISMISS_HELP = 'DISMISS_HELP';
 export const handleDismissHelp = () => ({
   type: DISMISS_HELP,
 });
+
+export const FETCH_DESCRIPTION_SUCCESS = 'FETCH_DESCRIPTION_SUCCESS';
+export const fetchDescriptionSuccess = (repository, description) => ({
+  type: FETCH_DESCRIPTION_SUCCESS,
+  repository,
+  description,
+});
+
+export const FETCH_DESCRIPTION_ERROR = 'FETCH_DESCRIPTION_ERROR';
+export const fetchDescriptionError = (repository, error) => ({
+  type: FETCH_DESCRIPTION_ERROR,
+  repository,
+  error,
+});
+
+export const fetchDescription = repository => (dispatch) => {
+  const url = `https://api.github.com/repos/${repository}`;
+  return fetch(url).then((response) => {
+    if (!response.ok) {
+      const error = new Error(response.statusText);
+      error.response = response;
+      throw error;
+    }
+    return response;
+  })
+    .then(response => response.json())
+    .then(data => dispatch(fetchDescriptionSuccess(repository, data.description)))
+    .catch(error => dispatch(fetchDescriptionError(repository, error)));
+};
