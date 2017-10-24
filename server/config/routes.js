5714e39a1ccbb8681e2f755519ea3175f499fdd5
johnvincentio <john@johnvincent.io>
2017-10-24 11:22:54 -0400

    travis

--- server/config/routes.js
+++ server/config/routes.js
@@ -6,6 +6,7 @@
  * @requires scoreRouter
  */
 
+const path = require('path');
 const scoreRouter = require('../api/score/score.router');
 
 const errors = require('./errors');
@@ -13,8 +14,12 @@ const errors = require('./errors');
 module.exports = (app) => {
 	app.use('/api/score', scoreRouter);
 
-	app.use('*', (req, res) => {
-		res.status(404).json({ message: 'Not Found' });
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 	});
+
+	// app.use('*', (req, res) => {
+	// 	res.status(404).json({ message: 'Not Found' });
+	// });
 	app.use(errors);
 };
